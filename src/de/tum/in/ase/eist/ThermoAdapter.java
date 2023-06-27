70a973111ea027fa7461077f80b6c195b03e8317
Egemen Baris Durmaz <ge85vet@mytum.de>
2023-06-27 21:46:16 +0200

    Add an association in ThermoAdapter

--- src/de/tum/in/ase/eist/ThermoAdapter.java
+++ src/de/tum/in/ase/eist/ThermoAdapter.java
@@ -1,6 +1,7 @@
 package de.tum.in.ase.eist;
 
 public class ThermoAdapter implements ThermoInterface {
+    FahrenheitThermo thermo = new FahrenheitThermo();
     @Override
     public double getTempC() {
         return 0;
