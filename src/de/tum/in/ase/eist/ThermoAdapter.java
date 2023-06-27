56a2f05c3341bb7c8bdc92049439c247e3594f50
Egemen Baris Durmaz <ge85vet@mytum.de>
2023-06-27 21:51:04 +0200

    Implement the method in ThermoAdapter

--- src/de/tum/in/ase/eist/ThermoAdapter.java
+++ src/de/tum/in/ase/eist/ThermoAdapter.java
@@ -4,7 +4,7 @@ public class ThermoAdapter implements ThermoInterface {
     private FahrenheitThermo thermo = new FahrenheitThermo();
     @Override
     public double getTempC() {
-        return 0;
+        return (thermo.getFahrenheitTemperature() - 32) * 5 / 9;
     }
 
 }
